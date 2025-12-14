d3439423fab9d5ba2b7dbd8905070bf06143ec33
IvanPratasevich <iv.pratasevich@gmail.com>
2025-12-14 16:07:01 +0300

    refactor: delete files

--- tests/mainPage.spec.js
+++ tests/mainPage.spec.js
@@ -1,25 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { AdBlock } from '../src/utils';
-import { MainPage } from '../src/pageObjects';
 
 test.beforeEach(async ({ page }) => {
   await AdBlock.blockAds(page);
   await page.goto('https://demoqa.com', { waitUntil: 'domcontentloaded' });
-});
-test.describe('Main @entity_18', () => {
-  test('Has header banner', async ({ page }) => {
-    const mainPage = new MainPage(page);
-    const headerBanner = mainPage.headerLocator;
-    await expect(headerBanner).toBeVisible();
-
-    await test.step('Elements Card visible in main page', async () => {
-      await mainPage.checkCategoryCard('Elements');
-    });
-    await test.step('Forms Card visible in main page', async () => {
-      await mainPage.checkCategoryCard('Forms');
-    });
-    await test.step('Widgets Card visible in main page', async () => {
-      await mainPage.checkCategoryCard('Widgets');
-    });
-  });
 });
